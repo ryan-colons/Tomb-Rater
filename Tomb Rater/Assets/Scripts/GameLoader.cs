8634ec7fd4b3ed6b814e33a85979e7a74948689e
rycoll <colry349@gmail.com>
2017-11-14 14:16:46 +1300

    Made a basic title menu

---
+++ Tomb Rater/Assets/Scripts/GameLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour {
+
+	public GameObject gameControllerPrefab;
+
+	private void Start () {
+		if (GameObject.FindGameObjectsWithTag ("GameController").Length == 0) {
+			Instantiate (gameControllerPrefab, this.transform.position, Quaternion.identity);
+		}
+	}
+
+	public void startNewGame () {
+		Debug.Log ("Starting a new game!");
+	}
+
+	public void continueGame () {
+		Debug.Log ("Continuing a previous game!");
+	}
+
+	public void exitApplication () {
+		Application.Quit ();
+	}
+}
